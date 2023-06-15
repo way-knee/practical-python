fcdd5dccac5d526dc90bd2b8dbeb829e8317f1fe
way-knee <winnie.chen.chouchou@gmail.com>
2023-06-15 15:18:14 -0400

    add class folder

---
+++ Work/Class/stock.py
@@ -0,0 +1,23 @@
+class Stock:
+    def __init__(self, name, shares, price):
+        self.name = name
+        self.shares = shares
+        self.price = price
+        
+    def cost(self):
+        return self.shares * self.price
+
+    def sell(self, nshares):
+        self.shares -= nshares
+
+class MyStock(Stock):
+    def __init__(self, name, shares, price, factor):
+        super().__init__(name, shares, price)
+        self.factor = factor
+
+    def panic(self):
+        self.sell(self.shares)
+
+    def cost(self):
+        actual_cost = super().cost()
+        return self.factor * actual_cost
---
+++ Work/player.py
@@ -0,0 +1,15 @@
+class Player:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+        self.health = 100
+
+    def move(self, dx, dy):
+        self.x += dx
+        self.y += dy
+
+    def damage(self, pts):
+        self.health -= pts
+
+    def left(self, amt):
+        self.move(-amt, 0)
