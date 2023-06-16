1626250238fdfe159ff98d018ecc548b0a728f4a
way-knee <winnie.chen.chouchou@gmail.com>
2023-06-16 10:48:31 -0400

    class, special methods

--- Work/Class/tableformat.py
+++ Work/Class/tableformat.py
@@ -1,4 +1,18 @@
-class TextTableFormatter:
+class TableFormatter:
+    def headings(self, headers):
+        '''
+        Emint the table headers
+        '''
+        raise NotImplementedError()
+
+    def row(self, rowdata):
+        '''
+        Emint a single row of table data
+        '''
+        raise NotImplementedError()
+
+
+class TextTableFormatter(TableFormatter):
     def headings(self, headers):
         for h in headers:
             print(f'{h:>10s}', end=' ')
@@ -11,7 +25,7 @@ class TextTableFormatter:
         print()
 
 
-class CSVTableFormatter(TextTableFormatter):
+class CSVTableFormatter(TableFormatter):
     def headings(self, headers):
         print(','.join(headers))
 
@@ -19,7 +33,7 @@ class CSVTableFormatter(TextTableFormatter):
         print(','.join(rowdata))
 
 
-class HTMLTableFormatter(TextTableFormatter):
+class HTMLTableFormatter(TableFormatter):
     def headings(self, headers):
         print('<tr>', end='')
         for h in headers:
@@ -42,4 +56,9 @@ def create_formatter(name):
         return HTMLTableFormatter()
     else:
         raise RuntimeError(f'Unknown format {name}')
-    
+
+def print_table(objects, headers, formatter):
+    formatter.headings(headers)
+    for obj in objects:
+        rowdata = [ str(getattr(obj, h)) for h in headers ]
+        formatter.row(rowdata)
